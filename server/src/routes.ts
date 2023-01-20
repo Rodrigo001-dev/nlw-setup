76f61be8fb4c2879f6b141a10998d37af1307ee0
Rodrigo <rodrigorael53@gmail.com>
2023-01-20 01:48:29 -0300

    feat: :sparkles: create a complex query in summary

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,6 @@
 import dayjs from "dayjs";
 import { FastifyInstance } from "fastify";
-import { z } from "zod";
+import { date, z } from "zod";
 
 import { prisma } from "./lib/prisma";
 
@@ -76,5 +76,83 @@ export async function appRoutes(app: FastifyInstance) {
     });
 
     return { possibleHabits, completedHabits };
+  });
+
+  // essa rota vai mudar um status de um hábito, se ele está completo ou não
+  // considerando a semântica do método patch, faz sentido utilizar ele aqui
+  // o patch é utilizado para mudar informações pequenas(com um status)
+  app.patch("/habits/:id/toggle", async (request) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = toggleHabitParams.parse(request.params);
+
+    const today = dayjs().startOf("day").toDate();
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      },
+    });
+
+    // se eu não encontrar o dia, ou seja, se esse dia não estiver cadastrado no
+    // banco de dados quer dizer que pessoa não tinha completado nenhum hábito
+    // ainda
+    if (!day) {
+      day = await prisma.day.create({
+        data: { date: today },
+      });
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    });
+
+    // se ele encontrou esse registrou, quer dizer que o usuário tinha marcado
+    // o hábito como completo
+    if (dayHabit) {
+      // remover a marcação como completo
+      await prisma.dayHabit.delete({
+        where: { id: dayHabit.id },
+      });
+    } else {
+      // completar o hábito nesse dia pela primeira vez
+      await prisma.dayHabit.create({
+        data: { day_id: day.id, habit_id: id },
+      });
+    }
+  });
+
+  app.get("/summary", async () => {
+    const summary = await prisma.$queryRaw`
+      SELECT 
+        D.id,
+        D.date,
+        (
+          SELECT 
+            cast(count(*) as float)
+          FROM days_habits DH
+          WHERE DH.day_id = D.id
+        ) as completed,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM habit_week_days HWD
+          JOIN habits H
+            ON H.id = HWD.habit_id
+          WHERE
+            HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+            AND H.created_at <= D.date
+        ) as amount
+      FROM days D
+    `;
+
+    return summary;
   });
 }
