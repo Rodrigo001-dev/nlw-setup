e001d1613a38fefd4e1002b7928c7b85995c5005
Rodrigo <rodrigorael53@gmail.com>
2023-01-21 15:54:02 -0300

    :bug: style: add bg zinc 900

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -54,7 +54,7 @@ export function New() {
         </Text>
 
         <TextInput
-          className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white border-2 border-zinc-800 focus:border-green-600"
+          className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
           placeholder="Exercício, dormir bem, etc..."
           placeholderTextColor={colors.zinc[400]}
         />
