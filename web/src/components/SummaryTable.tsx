4dcee10497a3ab29a996e3fe233f54fff16f2ee0
Rodrigo <rodrigorael53@gmail.com>
2023-01-21 02:37:25 -0300

    :lipstick: style: add style dynamically

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -26,7 +26,13 @@ export function SummaryTable() {
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map((date) => {
-          return <HabitDay key={date.toString()} />;
+          return (
+            <HabitDay
+              amount={5}
+              completed={Math.round(Math.random() * 5)}
+              key={date.toString()}
+            />
+          );
         })}
 
         {amountOfDaysToFill > 0 &&
