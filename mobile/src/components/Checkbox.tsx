51264ef06768f9b919fb68a061910323933c75f4
Rodrigo <rodrigorael53@gmail.com>
2023-01-21 15:50:28 -0300

    :bug: style: add font semibold

--- mobile/src/components/Checkbox.tsx
+++ mobile/src/components/Checkbox.tsx
@@ -27,7 +27,7 @@ export function Checkbox({ title, checked = false, ...rest }: Props) {
         <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
       )}
 
-      <Text className="text-white text-base ml-3">{title}</Text>
+      <Text className="text-white text-base ml-3 font-semibold">{title}</Text>
     </TouchableOpacity>
   );
 }
