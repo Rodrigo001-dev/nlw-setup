df5695ac9e843a0371ad6410afb2a13603ade896
Rodrigo <rodrigorael53@gmail.com>
2023-01-22 16:40:31 -0300

    :bug: fix: ampty array when completedHabits empty

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,6 @@ const app = Fastify();
 app.register(cors);
 app.register(appRoutes);
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
   console.log("HTTP Server running!");
 });
--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,10 +1,9 @@
 import * as Popover from "@radix-ui/react-popover";
-import * as Checkbox from "@radix-ui/react-checkbox";
 import dayjs from "dayjs";
-import { Check } from "phosphor-react";
 import clsx from "clsx";
 
 import { ProgressBar } from "./ProgressBar";
+import { HabitsList } from "./HabitsList";
 
 interface HabitDayProps {
   date: Date;
@@ -45,20 +44,7 @@ export function HabitDay({ completed = 0, amount = 0, date }: HabitDayProps) {
 
           <ProgressBar progress={completedPercentage} />
 
-          <div className="mt-6 flex flex-col gap-3">
-            {/* com o group eu consigo estilizar outros elementos a partir das propriedades que eu defini esse group(no caso eu vou conseguir estilizar com qualquer propriedade que o Checkbox do radix recebe) */}
-            <Checkbox.Root className="flex items-center gap-3 group">
-              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
-                <Checkbox.Indicator>
-                  <Check size={20} className="text-white" />
-                </Checkbox.Indicator>
-              </div>
-
-              <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
-                Beber 2L de água
-              </span>
-            </Checkbox.Root>
-          </div>
+          <HabitsList date={date} />
 
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
