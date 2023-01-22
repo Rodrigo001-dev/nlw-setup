ee6a0ae1e7661a6fb04b131ff7ed521325c72332
Rodrigo <rodrigorael53@gmail.com>
2023-01-22 01:56:50 -0300

    :sparkles: feat: add the weekDays in form

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,7 +1,9 @@
 import * as Popover from "@radix-ui/react-popover";
+import * as Checkbox from "@radix-ui/react-checkbox";
 import clsx from "clsx";
 
 import { ProgressBar } from "./ProgressBar";
+import { Check } from "phosphor-react";
 
 interface HabitDayProps {
   completed: number;
@@ -38,6 +40,21 @@ export function HabitDay(props: HabitDayProps) {
           </span>
 
           <ProgressBar progress={completedPercentage} />
+
+          <div className="mt-6 flex flex-col gap-3">
+            {/* com o group eu consigo estilizar outros elementos a partir das propriedades que eu defini esse group(no caso eu vou conseguir estilizar com qualquer propriedade que o Checkbox do radix recebe) */}
+            <Checkbox.Root className="flex items-center gap-3 group">
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
+
+              <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
+                Beber 2L de água
+              </span>
+            </Checkbox.Root>
+          </div>
 
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
