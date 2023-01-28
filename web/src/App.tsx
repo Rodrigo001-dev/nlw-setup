ef1b86b4b83fa173ba0c7f9d15726c6d5f093714
Rodrigo <rodrigorael53@gmail.com>
2023-01-28 18:53:05 -0300

    :sparkles: feat: implement push notification frontend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,37 @@
 import { Header } from "./components/Header";
 import { SummaryTable } from "./components/SummaryTable";
 
+import { api } from "./lib/axios";
+
 import "./lib/dayjs";
 import "./styles/global.css";
+
+navigator.serviceWorker
+  .register("service-worker.js")
+  .then(async (serviceWorker) => {
+    // a subscription é a assinatura do usuário com o serviço de notificação, ou seja,
+    // quando o usuário falar que quer receber notificação vai ser criado uma subscription
+    let subscription = await serviceWorker.pushManager.getSubscription();
+
+    // se ele não tiver uma subscription ativa
+    if (!subscription) {
+      const publicKeyResponse = await api.get("/push/public_key");
+
+      // vou criar uma nova subscription
+      subscription = await serviceWorker.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: publicKeyResponse.data.publicKey,
+      });
+    }
+
+    await api.post("/push/register", {
+      subscription,
+    });
+
+    await api.post("/push/send", {
+      subscription,
+    });
+  });
 
 export function App() {
   return (
